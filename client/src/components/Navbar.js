97b70cc37949e1c7f28367ca63ccfb73b59bd346
Rayesh <rayeshrajah@gmail.com>
2020-04-11 02:06:00 +0000

    added flex to the nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,10 +4,12 @@ import Button  from './Button'
 
 export default function Navbar(props){
     return(
-    <nav class="navbar navbar-light bg-light">
+    <nav class="navbar navbar-light bg-light" id="navbar">
     <a class="navbar-brand" id="logo-text">SpoilerBoiler</a>
+    <div id="nav-user-info">
     <a><Button login onClick={props.login}>Login</Button></a>
     <a><Button signup onClick={props.signup}>Sign-up</Button></a>
+    </div>
     </nav>
     );
 }
