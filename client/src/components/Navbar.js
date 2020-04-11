1bd34dcf464ed03fb2fa9804f2d40061cc17add5
Rayesh <rayeshrajah@gmail.com>
2020-04-11 00:05:53 +0000

    Building NavBar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar(props){
+    return(
+        <ul>
+         <li>
+            Login
+         </li>
+         <li>
+            Sign-up
+         </li>
+         </ul>
+    );
+}
