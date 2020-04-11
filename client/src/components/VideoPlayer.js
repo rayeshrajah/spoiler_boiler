668b26cfd0a15e29e6ea0f1d4dc4d5979761b978
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-11 16:08:40 -0400

    display comment ticks on spoiler-timeline

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -4,10 +4,11 @@ import '../styles/VideoPlayer.scss'
  
 function VideoPlayer(props) {
   const [progress, setProgress] = useState("")
-  const [timestamp, setTimestamp] = useState(props)
+  const [timestamp, setTimestamp] = useState(props.commentTimestamps)
+  const [videoDuration, setVideoDuration] = useState(0)
   const reactPlayerLib = useRef(null)
 
-  // console.log(props)
+  const commentTick = Math.floor((timestamp[1] / videoDuration) * 100) + "%"
 
   return (
     <div className="video-master">
@@ -15,19 +16,20 @@ function VideoPlayer(props) {
         <ReactPlayer
           ref={reactPlayerLib}
           className="video" 
-          url='https://www.youtube.com/watch?v=ysz5S6PUM-U' 
+          url='https://www.youtube.com/watch?v=7d7-etf-wNI' 
           playing 
           controls
           onProgress={(obj) => setProgress(Math.floor(obj.played * 100) + "%")}
-          onDuration={(duration) => console.log(duration)}
+          onDuration={(duration) => setVideoDuration(duration)}
         />
       </div>
       <div className="spoiler-timebar-master">
         <div className="spoiler-bar">
-          <div className="spoiler-time" style={{width: String(progress)}}></div>
+          <div className="spoiler-time" style={{width: String(progress)}}>
+            <div className="comment-ticks" style={{left: String(commentTick)}}></div>
+          </div>
         </div>
       </div>
-      <div></div>
     </div>
   )
 }
