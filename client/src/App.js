d318a407cb379b910e172c202e12b62c534f9e48
Rayesh <rayeshrajah@gmail.com>
2020-04-11 02:55:01 +0000

    Added some css to the buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './styles/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios'
