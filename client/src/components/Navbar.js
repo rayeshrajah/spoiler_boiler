0231a1955d7e917f1e2f5a4a0e3fb715527505c5
Rayesh <rayeshrajah@gmail.com>
2020-04-11 01:05:26 +0000

    added scss variables and color to the title of app

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../styles/Navbar.scss'
 
 export default function Navbar(){
     return(
     <nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand">SpoilerBoiler</a>
+    <a class="navbar-brand" id="logo-text">SpoilerBoiler</a>
     </nav>
     );
 }
