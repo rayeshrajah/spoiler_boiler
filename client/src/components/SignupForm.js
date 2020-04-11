32e15524107d5619ddb6b9877ad0e4bd81880bdd
Rayesh <rayeshrajah@gmail.com>
2020-04-11 17:29:02 +0000

    signup form button styled

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -7,16 +7,16 @@ export default function SignupForm(){
     <form>
     <div class="form-group">
     <label for="username" class="name-label">Name:</label>
-    <input type="text" class="form-control" id="username" placeholder="Enter a username"/>
+    <input type="text" class="form-control" id="username"/>
     </div>
     <div class="form-group">
     <label for="InputEmail" class="email-label">Email address:</label>
-    <input type="email" class="form-control" id="EmailInput" placeholder="Enter email"/>
+    <input type="email" class="form-control" id="EmailInput"/>
     </div>
     <div class="form-group">
     <label for="InputPassword" class="password-label">Password:</label>
-    <input type="password" class="form-control" id="PasswordInput" placeholder="Password"/>
+    <input type="password" class="form-control" id="PasswordInput"/>
     </div>
-    <Button signup type="submit"></Button>
+    <Button signup type="submit">Submit</Button>
     </form>);
 }
