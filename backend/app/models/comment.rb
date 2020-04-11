9c8d51896f2272a8a700b0dbd130c10e51a4e453
Rayesh <rayeshrajah@gmail.com>
2020-04-11 17:45:20 +0000

    added validations for comments and video active records

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+    validates :message, presence: true
+    validates :timestamp_in_seconds, presence:true
 end
--- backend/app/models/video.rb
+++ backend/app/models/video.rb
@@ -1,2 +1,5 @@
 class Video < ApplicationRecord
+    validates :video_url, presence :true
+    validates :name, presence :true
+    validates :url_id, prensence :true
 end
