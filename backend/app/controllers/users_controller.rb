bd0d73cec7d918464841664a02369ee48bbb7ce1
Rayesh <rayeshrajah@gmail.com>
2020-04-12 00:50:27 +0000

    user can now register

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,23 +1,19 @@
 class UsersController < ApplicationController
     def index
-       @users = User.all
-       render json: {status: "sucess", message: "loaded users", data: @users}, status: :ok
+       users = User.all
+       render json: users
     end
     def new
-        @user = User.new
+        user = User.new
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: {status: "sucess", message: "created users", data: @user}, status: :ok   
-        else
-            render json: {status: "error", message: "Did not create user", data: @user.errors}, status: :unprocessable_entity   
-        end
+        user = User.new(user_params)
+        user.save
     end
     
     private 
     def user_params 
-        params.permit(:name, :email, :password)
+        params.require(:user).permit(:name, :email, :password)
     end
 end
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import '../styles/SignupForm.scss'
 import Button from './Button'
 import axios from 'axios'
 
-export default function SignupForm(props){
-    const [info, setInfo] = useState({name: "", email: "", password: "" })
-    const {name, email, password} = info
+export default function SignupForm(){
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     function userInfo(){
      return axios.post('/users', {
-         headers: {"Content-Type": "application/json"},
-         data: {
-             "name": "ray",
-             "email": "ray@something.com",
-             "password": "secret"
+         user: {
+             "name": name,
+             "email": email,
+             "password": password
          }
-     })
-                 .then((response) => console.log(response.data))
-                 .catch((error) => console.log(error))
+     }).then((response) => console.log(response))
+       .catch((error) => console.log(error))
     }
-    console.log(info)
+    console.log(name)
+    console.log(email)
+    console.log(password)
     return (
     <form onSubmit = {(event) => event.preventDefault()}>
     <section id= "signup-form">
@@ -29,7 +30,7 @@ export default function SignupForm(props){
            id="username" 
            name="name"
            value={name} 
-           onChange={(event) => setInfo(event.target.value)}/>
+           onChange={(event) => setName(event.target.value)}/>
     </div>
     <div class="form-group">
     <label for="InputEmail" class="email-label">Email address:</label>
@@ -38,7 +39,7 @@ export default function SignupForm(props){
            id="EmailInput"
            name="email" 
            value={email}
-           onChange={(event) => setInfo(event.target.value)}  
+           onChange={(event) => setEmail(event.target.value)}  
            />
     </div>
     <div class="form-group">
@@ -48,7 +49,7 @@ export default function SignupForm(props){
            id="PasswordInput"
            name="password"
            value={password}
-           onChange={(event) => setInfo(event.target.value)}
+           onChange={(event) => setPassword(event.target.value)}
            />
     </div>
     <Button signup onClick={userInfo}>Submit</Button>
