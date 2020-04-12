281c3d849af25843262c294db8558c12c73dfa84
Eric-Lombardo <ejlanim8or@gmail.com>
2020-04-11 22:21:20 -0400

    add html/css mockup for comment placement

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import '../styles/Comments.scss'
 
 function Comments(props) {
 
 
   return (
-    <div>
-      hello!!!!
+    <div className="comments-master-container">
+      <div className="individual-comment">
+        <img src="https://www.placecage.com/300/300"/>
+        <p>lorem ipsum blah blah ....</p>
+      </div>
     </div>
   )
 }
