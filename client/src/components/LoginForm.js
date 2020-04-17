85e558a3423a70211ced312ebbfc87e99cdfbc31
Rayesh <rayeshrajah@gmail.com>
2020-04-17 13:50:41 -0400

    fixed user login that made our database run while

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import Button from './Button'
 import '../styles/Form.scss'
@@ -7,61 +7,58 @@ export default function LoginForm(props) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [userInfo, setUserInfoFound] = useState(false)
-  
-  useEffect(()=>{
+
+  function userInfoFound() {
     axios.get('/users')
-         .then( (response) => {
-             let data = response.data
-             data.forEach(element => {
-                 if(element.email === email && element.password === password){
-                   setUserInfoFound(true)
-                   props.loggedIn(element.id)
-                 }
-                });
-            })
-            .catch(error => console.log(error))
-        })
-        
-    function userInfoFound(){
-        if(userInfo){
-        props.displayState(false)
-        return props.displayButton(false)
-      }
+      .then((response) => {
+        let data = response.data
+        data.forEach(element => {
+          if (element.email === email && element.password === password) {
+            setUserInfoFound(true)
+            props.loggedIn(element.id)
+            props.displayState(false)
+            props.displayButton(false)
+          }
+        });
+      })
+      .catch(error => console.log(error))
   }
 
+
+
   return (
-    <form onSubmit = {(event) => event.preventDefault()}>
-    <section className="user-form">
-      <div class="form-group">
-        <label for="InputEmail" class="email-label">
-          Email address:
+    <form onSubmit={(event) => event.preventDefault()}>
+      <section className="user-form">
+        <div class="form-group">
+          <label for="InputEmail" class="email-label">
+            Email address:
         </label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      <div class="form-group">
-        <label for="InputPassword" class="password-label">
-          Password:
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div class="form-group">
+          <label for="InputPassword" class="password-label">
+            Password:
         </label>
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          name="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-       <Button login onClick={userInfoFound}>
-        Submit
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <Button login onClick={userInfoFound}>
+          Submit
       </Button>
-    </section>
+      </section>
     </form>
   );
 }
